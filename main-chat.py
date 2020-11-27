68cf0bf1487f767abe8b5ee4979d0d4c1bfdc91e
amar0898 <70146865+amar0898@users.noreply.github.com>
2020-11-27 12:55:38 +0530

    Update main-chat.py

--- main-chat.py
+++ main-chat.py
@@ -10,12 +10,12 @@ from flask_socketio import SocketIO,send,join_room,leave_room
 
 app = Flask(__name__)
 
-app.secret_key ='amar'
+app.secret_key = os.environ.get('SECRET_KEY')
 
 
 
 app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get('DATABASE_URL')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
 
 db = SQLAlchemy(app)
 
